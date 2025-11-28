61414f7c824048a2be5e8aa5a52d885b2b1bf59a
Gercek Kriptocu <110336938+Gercekkriptocu@users.noreply.github.com>
2025-11-28 16:57:08 +0300

    Add next.config.ts

---
+++ next.config.ts
@@ -0,0 +1,17 @@
+import type { NextConfig } from 'next';
+
+const nextConfig: NextConfig = {
+  webpack: (config) => {
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      'pino-pretty': false,
+      '@react-native-async-storage/async-storage': false,
+    };
+    
+    config.externals.push('pino-pretty', 'encoding');
+    
+    return config;
+  },
+};
+
+export default nextConfig;
